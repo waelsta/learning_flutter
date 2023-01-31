8108018af20d27ba2f0706b35999731f62017b32
waelsta <waelsta055@gmail.com>
2023-01-31 23:20:11 +0100

    basic body layout

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -19,8 +19,31 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground!'),
+      body: Column(
+        children: <Widget>[
+          Container(
+            child: Card(
+              color: Colors.amber,
+              child: Text(
+                'Chart',
+                textAlign: TextAlign.center,
+              ),
+              elevation: 5,
+            ),
+            color: Colors.amber[100],
+            width: double.infinity,
+            height: 100,
+          ),
+          Container(
+            child: Card(
+              child: Text('list of transacitons'),
+              margin: EdgeInsets.only(top: 20),
+              color: Colors.blue,
+            ),
+            width: double.infinity,
+            height: 200,
+          )
+        ],
       ),
     );
   }
